5d474c1b9bf44488ed4e26bc40a642cf566f4a70
Thein Than Hein <thein.than.hein.th@gmail.com>
2022-05-13 18:02:24 -0700

    seperate into smaller components profile

--- src/components/Profile/Shortcuts.js
+++ src/components/Profile/Shortcuts.js
@@ -1,9 +1,6 @@
-import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
-import FaceIcon from "@mui/icons-material/Face";
-import AudiotrackIcon from "@mui/icons-material/Audiotrack";
-import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
 import { useState } from "react";
 import { Tab, Tabs } from "@mui/material";
+import shortcuts from "../../data/shortcuts";
 
 const Shortcuts = () => {
   const [value, setValue] = useState(0);
@@ -31,70 +28,25 @@ const Shortcuts = () => {
         },
       }}
     >
-      <Tab
-        disableRipple
-        disableTouchRipple
-        icon={<PhotoLibraryIcon />}
-        iconPosition="start"
-        label="Photos"
-        sx={{
-          margin: "0 5px",
-          textTransform: "capitalize",
-          bgcolor: "#f1f3f4",
-          borderRadius: "9999px",
-          padding: "0 15px",
-          minHeight: "fit-content",
-          height: "30px",
-        }}
-      />
-      <Tab
-        disableRipple
-        disableTouchRipple
-        icon={<FaceIcon />}
-        iconPosition="start"
-        label="Avatars"
-        sx={{
-          margin: "0 5px",
-          textTransform: "capitalize",
-          bgcolor: "#f1f3f4",
-          borderRadius: "9999px",
-          padding: "0 15px",
-          minHeight: "fit-content",
-          height: "30px",
-        }}
-      />
-      <Tab
-        disableRipple
-        disableTouchRipple
-        icon={<AudiotrackIcon />}
-        iconPosition="start"
-        label="Music"
-        sx={{
-          margin: "0 5px",
-          textTransform: "capitalize",
-          bgcolor: "#f1f3f4",
-          borderRadius: "9999px",
-          padding: "0 15px",
-          minHeight: "fit-content",
-          height: "30px",
-        }}
-      />
-      <Tab
-        disableRipple
-        disableTouchRipple
-        icon={<EmojiObjectsIcon />}
-        iconPosition="start"
-        label="Did you know"
-        sx={{
-          margin: "0 5px",
-          textTransform: "capitalize",
-          bgcolor: "#f1f3f4",
-          borderRadius: "9999px",
-          padding: "0 15px",
-          minHeight: "fit-content",
-          height: "30px",
-        }}
-      />
+      {shortcuts.map(({ id, name, icon }) => (
+        <Tab
+          disableRipple
+          disableTouchRipple
+          icon={icon}
+          iconPosition="start"
+          label={name}
+          key={id}
+          sx={{
+            margin: "0 5px",
+            textTransform: "capitalize",
+            bgcolor: "#f1f3f4",
+            borderRadius: "9999px",
+            padding: "0 15px",
+            minHeight: "fit-content",
+            height: "30px",
+          }}
+        />
+      ))}
     </Tabs>
   );
 };
